0275291706b76161fd15b18273f3c291b16817fa
fumihachi94 <fumiya.sato.rop@gmail.com>
2023-05-20 20:51:42 +0900

    add abc_299 temp

---
+++ abc_299/A.cpp
@@ -0,0 +1,41 @@
+﻿#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+#include <fstream> //提出時は削除
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, c, n) for (ll i = c; i < (n); ++i)
+using ll = long long;
+#define rep(i,n) for(ll i = 0; i < (n); ++i)
+
+int main()
+{
+    std::ifstream in("input.txt"); //提出時は削除
+    std::cin.rdbuf(in.rdbuf()); //提出時は削除
+
+    int length;
+    cin >> length;
+
+    cout << length << endl;
+}
